ce046a3d6bbaa415fb2f977248923c54d78ff1a4
Alessandro Anelli <127589937+APA2306@users.noreply.github.com>
2023-12-20 09:59:40 +0100

    .

---
+++ Anelli.Alessandro.4I.SlotMachine/Anelli.Alessandro.4I.CliSlotMachine/Program.cs
@@ -0,0 +1,5 @@
+﻿using Anelli.Alessandro._4I.LibSlotMachine;
+
+Console.WriteLine("Hello, SlotMachine!");
+
+SlotMachine S = new SlotMachine(10);
---
+++ Anelli.Alessandro.4I.SlotMachine/Anelli.Alessandro.4I.LibSlotMachine/SlotMachine.cs
@@ -0,0 +1,17 @@
+﻿namespace Anelli.Alessandro._4I.LibSlotMachine
+{
+    public class SlotMachine
+    {
+        public int Credito { get; set; }
+
+        public SlotMachine() 
+        {
+            Credito = 0;
+        }
+        public SlotMachine(int credito)
+        {
+            Credito = credito;
+        }
+        
+    }
+}
